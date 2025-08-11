55cc094ec081f85371f179210f69d6c6c75a88f8
Christoph Schmatzler <christoph@tuist.dev>
2025-08-11 15:08:28 +0200

    up

--- profiles/base/home-manager/mise.nix
+++ profiles/base/home-manager/mise.nix
@@ -5,9 +5,10 @@
     enableZshIntegration = true;
     globalConfig = {
       env = {
-        KERL_CONFIGURE_OPTIONS = "--with-termcap";
-        CPPFLAGS = "-I${pkgs.ncurses.dev}/include";
-        LDFLAGS = "-L${pkgs.ncurses.out}/lib";
+        KERL_CONFIGURE_OPTIONS = "--with-termcap --with-ssl=${pkgs.openssl.dev}";
+        CPPFLAGS = "-I${pkgs.ncurses.dev}/include -I${pkgs.openssl.dev}/include";
+        LDFLAGS = "-L${pkgs.ncurses.out}/lib -L${pkgs.openssl.out}/lib";
+        PKG_CONFIG_PATH = "${pkgs.ncurses.dev}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig";
       };
     };
   };
