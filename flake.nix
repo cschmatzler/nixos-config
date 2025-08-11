56eb83f0dae4243b757cfdfb2f7fb11c63151484
Christoph Schmatzler <christoph@tuist.dev>
2025-08-11 14:24:47 +0200

    up

--- flake.nix
+++ flake.nix
@@ -84,9 +84,6 @@
               modules = [
                 inputs.disko.nixosModules.disko
                 inputs.home-manager.nixosModules.home-manager
-                {
-                  nixpkgs.config.allowUnfree = true;
-                }
                 ./hosts/nixos/${hostname}
               ];
             }
