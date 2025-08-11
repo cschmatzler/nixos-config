96e7b07d4b9ac35f4302ff746da043e8a9af0651
Christoph Schmatzler <christoph@tuist.dev>
2025-08-11 15:04:41 +0200

    up

--- profiles/base/home-manager/mise.nix
+++ profiles/base/home-manager/mise.nix
@@ -1,7 +1,14 @@
-{
+{pkgs, ...}: {
   programs.mise = {
     enable = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+    globalConfig = {
+      env = {
+        KERL_CONFIGURE_OPTIONS = "--with-termcap";
+        CPPFLAGS = "-I${pkgs.ncurses.dev}/include";
+        LDFLAGS = "-L${pkgs.ncurses.out}/lib";
+      };
+    };
   };
 }
