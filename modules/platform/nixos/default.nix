2a40e49f705702f55acc05754e59559226c4818d
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-12 19:10:37 +0000

    up

--- modules/platform/nixos/default.nix
+++ modules/platform/nixos/default.nix
@@ -5,7 +5,11 @@
   sops-nix,
   ...
 }: let
-  sshKeys = import ../../../shared/ssh-keys.nix;
+  sshKeys = {
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILw2lQn2yEwprOzz50kxG4fKXHzq6askh+XSGLSnWidd"
+    ];
+  };
 in {
   imports = [
     ../../core
--- shared/ssh-keys.nix
+++
@@ -1,5 +0,0 @@
-{
-  keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILw2lQn2yEwprOzz50kxG4fKXHzq6askh+XSGLSnWidd"
-  ];
-}
