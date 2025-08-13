7ac3a7899c0436deb9a0a274687cab43fb4ac242
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-13 18:52:29 +0000

    up

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -25,8 +25,8 @@
 
     settings = {
       devices = {
-        "jason" = {id = "42II2VO-QYPJG26-ZS3MB2I-AOPVZ67-JJNSE76-U54CO5Y-634A5OG-ECU4YQA";};
         "tahani" = {id = "6B7OZZF-TEAMUGO-FBOELXP-Z4OY7EU-5ZHLB5T-V6Z3UDB-Q2DYR43-QBYW6QM";};
+        "jason" = {id = "42II2VO-QYPJG26-ZS3MB2I-AOPVZ67-JJNSE76-U54CO5Y-634A5OG-ECU4YQA";};
       };
       folders = {
         "Projects" = {
@@ -34,7 +34,7 @@
             if pkgs.stdenv.isDarwin
             then "/Users/${user}/Projects"
             else "/home/${user}/Projects";
-          devices = [];
+          devices = ["tahani" "jason"];
         };
       };
       options.globalAnnounceEnabled = false;
