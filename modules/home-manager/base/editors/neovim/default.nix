c7f269343fbb56a6044854ceb02c2b112e4057da
Christoph Schmatzler <christoph@tuist.dev>
2025-08-13 08:27:12 +0200

    up

--- modules/home-manager/base/editors/neovim/default.nix
+++ modules/home-manager/base/editors/neovim/default.nix
@@ -3,13 +3,13 @@
     ./autocmd.nix
     ./mappings.nix
     ./options.nix
-    ./plugins/conform.nix
     ./plugins/blink-cmp.nix
+    ./plugins/conform.nix
     ./plugins/grug-far.nix
-    ./plugins/oil.nix
     ./plugins/lazygit.nix
     ./plugins/lsp.nix
     ./plugins/mini.nix
+    ./plugins/oil.nix
     ./plugins/treesitter.nix
   ];
 
@@ -17,7 +17,6 @@
     enable = true;
     defaultEditor = true;
     luaLoader.enable = true;
-
     colorschemes.catppuccin = {
       enable = true;
       settings = {
