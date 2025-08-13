30687f79492f642cb51969254aac3c3de9bc93c1
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-13 18:07:33 +0000

    fix casing

--- modules/home-manager/base/tools/bat.nix
+++ modules/home-manager/base/tools/bat.nix
@@ -13,7 +13,7 @@
           rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
           sha256 = "lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
         };
-        file = "Themes/Catppuccin Latte.tmTheme";
+        file = "themes/Catppuccin Latte.tmTheme";
       };
     };
   };
