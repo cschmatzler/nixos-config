1449b5a39d5e702721828e8a5bbf344057fe7e75
Christoph Schmatzler <christoph@tuist.dev>
2025-08-14 07:44:39 +0200

    up

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,18 +1,20 @@
 {
   user,
   pkgs,
-  lib,
   ...
 }: let
   isDarwin = pkgs.stdenv.isDarwin;
-  
-  platformConfig = if isDarwin then {
-    homeDir = "/Users/${user}";
-    group = "staff";
-  } else {
-    homeDir = "/home/${user}";
-    group = "users";
-  };
+
+  platformConfig =
+    if isDarwin
+    then {
+      homeDir = "/Users/${user}";
+      group = "staff";
+    }
+    else {
+      homeDir = "/home/${user}";
+      group = "users";
+    };
 in {
   services.syncthing = {
     enable = true;
@@ -29,11 +31,12 @@ in {
       devices = {
         "tahani" = {id = "6B7OZZF-TEAMUGO-FBOELXP-Z4OY7EU-5ZHLB5T-V6Z3UDB-Q2DYR43-QBYW6QM";};
         "jason" = {id = "42II2VO-QYPJG26-ZS3MB2I-AOPVZ67-JJNSE76-U54CO5Y-634A5OG-ECU4YQA";};
+        "chidi" = {id = "N7W6SUT-QO6J4BE-T3Y65SM-OFGYGNV-TGYBJPX-JVN4Z72-AENZ247-KWXOQA6";};
       };
       folders = {
         "Projects" = {
           path = "${platformConfig.homeDir}/Projects";
-          devices = ["tahani" "jason"];
+          devices = ["tahani" "jason" "chidi"];
         };
       };
       options.globalAnnounceEnabled = false;
