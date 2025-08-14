89938e355bfd1fef65c690fc3a940dad99553d86
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-14 12:21:52 +0000

    fix firewall

--- modules/networking/firewall.nix
+++ modules/networking/firewall.nix
@@ -1,6 +1,9 @@
-{
+{config, ...}: {
   networking.firewall = {
     enable = true;
     trustedInterfaces = ["eno1" "tailscale0"];
+    allowedUDPPorts = [config.services.tailscale.port];
+    allowedTCPPorts = [22];
+    checkReversePath = "loose";
   };
 }
