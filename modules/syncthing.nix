0f5419c1ddde2635464f3cf5077d78e5c4624f9f
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-15 08:52:22 +0000

    up

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -29,9 +29,18 @@ in {
 
     settings = {
       devices = {
-        "tahani" = {id = "6B7OZZF-TEAMUGO-FBOELXP-Z4OY7EU-5ZHLB5T-V6Z3UDB-Q2DYR43-QBYW6QM";};
-        "jason" = {id = "42II2VO-QYPJG26-ZS3MB2I-AOPVZ67-JJNSE76-U54CO5Y-634A5OG-ECU4YQA";};
-        "chidi" = {id = "N7W6SUT-QO6J4BE-T3Y65SM-OFGYGNV-TGYBJPX-JVN4Z72-AENZ247-KWXOQA6";};
+        "tahani" = {
+          id = "6B7OZZF-TEAMUGO-FBOELXP-Z4OY7EU-5ZHLB5T-V6Z3UDB-Q2DYR43-QBYW6QM";
+          addresses = ["tcp://tahani:22000"];
+        };
+        "jason" = {
+          id = "42II2VO-QYPJG26-ZS3MB2I-AOPVZ67-JJNSE76-U54CO5Y-634A5OG-ECU4YQA";
+          addresses = ["tcp://jason:22000"];
+        };
+        "chidi" = {
+          id = "N7W6SUT-QO6J4BE-T3Y65SM-OFGYGNV-TGYBJPX-JVN4Z72-AENZ247-KWXOQA6";
+          addresses = ["tcp://chidi:22000"];
+        };
       };
 
       folders = {
