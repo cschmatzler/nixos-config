0d7c23aeec90a515f13e363ec62a20b6c3a8742e
Christoph Schmatzler <christoph@schmatzler.com>
2025-09-04 08:09:30 +0200

    jj

--- modules/home/jujutsu.nix
+++ modules/home/jujutsu.nix
@@ -6,6 +6,13 @@
         name = "Christoph Schmatzler";
         email = "christoph@schmatzler.com";
       };
+      diff = {
+        tool = "delta";
+      };
+      ui = {
+        diff-formatter = ":git";
+        pager = ["delta" "--pager" "less -FRX"];
+      };
     };
   };
 }
