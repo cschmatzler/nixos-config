72048a3f5e2082581db0379a98dfc334f2b60f8d
Christoph Schmatzler <christoph@schmatzler.com>
2025-10-31 11:16:29 +0100

    up
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- hosts/jason/default.nix
+++ hosts/jason/default.nix
@@ -9,7 +9,22 @@
   services.syncthing.settings.folders = {
     "Projects/Personal" = {
       path = "/Users/${user}/Projects/Personal";
-      devices = ["tahani" "jason"];
+      devices = ["jason" "jason"];
+    };
+  };
+
+  sops.secrets = {
+    jason-syncthing-cert = {
+      sopsFile = ../../secrets/jason-syncthing-cert;
+      format = "binary";
+      owner = user;
+      path = "/Users/${user}/.config/syncthing/cert.pem";
+    };
+    jason-syncthing-key = {
+      sopsFile = ../../secrets/jason-syncthing-key;
+      format = "binary";
+      owner = user;
+      path = "/Users/${user}/.config/syncthing/key.pem";
     };
   };
 
