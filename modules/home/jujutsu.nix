49c4cbb245d69e8dcf0b02d704015d3bbc03d3cd
Christoph Schmatzler <christoph@schmatzler.com>
2025-11-13 15:56:17 +0000

    jj aliases

--- modules/home/jujutsu.nix
+++ modules/home/jujutsu.nix
@@ -15,6 +15,13 @@
 				pager = ["delta" "--pager" "less -FRX"];
 				diff-editor = ["nvim" "-c" "DiffEditor $left $right $output"];
 			};
+			aliases = {
+				tug = ["bookmark" "move" "--from" "closest_bookmark(@-)" "--to" "@-"];
+				retrunk = ["rebase" "-d" "trunk()"];
+			};
+			revset-aliases = {
+				"closest_bookmark(to)" = "heads(::to & bookmarks())";
+			};
 		};
 	};
 }
