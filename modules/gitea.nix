bcaeba6d8961b68a09399fbb307b609bbfdbbbf2
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-11 20:19:40 +0000

    gitea settings

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -15,12 +15,24 @@
 				HTTP_PORT = 3000;
 			};
 			service.DISABLE_REGISTRATION = true;
+			security.INSTALL_LOCK = true;
+			session = {
+				COOKIE_SECURE = true;
+				SAME_SITE = "strict";
+			};
+			api.ENABLE_SWAGGER = false;
 		};
 	};
 
 	services.caddy = {
 		enable = true;
 		virtualHosts."git.schmatzler.com".extraConfig = ''
+			header {
+				Strict-Transport-Security "max-age=31536000; includeSubDomains"
+				X-Content-Type-Options "nosniff"
+				X-Frame-Options "DENY"
+				Referrer-Policy "strict-origin-when-cross-origin"
+			}
 			reverse_proxy localhost:3000
 		'';
 	};
