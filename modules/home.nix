7eb591ba0cc447190ea07209dac4fb44708ab176
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-11 20:16:13 +0000

    format

--- modules/home.nix
+++ modules/home.nix
@@ -12,12 +12,13 @@ in {
 	home = {
 		packages = pkgs.callPackage ./packages.nix {inherit inputs;};
 		stateVersion = constants.stateVersions.homeManager;
-		activation = lib.mkIf pkgs.stdenv.isDarwin {
-			"setWallpaper" =
-				lib.hm.dag.entryAfter ["revealHomeLibraryDirectory"] ''
-					echo "[+] Setting wallpaper"
-					${setWallpaperScript}/bin/set-wallpaper-script
-				'';
-		};
+		activation =
+			lib.mkIf pkgs.stdenv.isDarwin {
+				"setWallpaper" =
+					lib.hm.dag.entryAfter ["revealHomeLibraryDirectory"] ''
+						echo "[+] Setting wallpaper"
+						${setWallpaperScript}/bin/set-wallpaper-script
+					'';
+			};
 	};
 }
