4c691c0cc1f78b79d07c62faaad705954be106c9
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-17 15:56:20 +0000

    deslop

--- profiles/neovim/plugins/jj-diffconflicts.nix
+++ profiles/neovim/plugins/jj-diffconflicts.nix
@@ -1,13 +1,14 @@
 {pkgs, ...}: {
 	programs.nixvim.extraPlugins = [
 		(pkgs.vimUtils.buildVimPlugin {
-			name = "jj-diffconflicts";
-			src = pkgs.fetchFromGitHub {
-				owner = "rafikdraoui";
-				repo = "jj-diffconflicts";
-				rev = "main";
-				hash = "sha256-FXsLSYy+eli8VArUL8ZOiPtyOk4Q8TUYwobEefZPRII=";
-			};
-		})
+				name = "jj-diffconflicts";
+				src =
+					pkgs.fetchFromGitHub {
+						owner = "rafikdraoui";
+						repo = "jj-diffconflicts";
+						rev = "main";
+						hash = "sha256-FXsLSYy+eli8VArUL8ZOiPtyOk4Q8TUYwobEefZPRII=";
+					};
+			})
 	];
 }
