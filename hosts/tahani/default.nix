3a9257725edc5a5ec2f48d180de99b7389311e96
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-20 21:47:20 +0000

    paperless

--- hosts/tahani/default.nix
+++ hosts/tahani/default.nix
@@ -129,6 +129,10 @@
 			owner = user;
 			path = "/home/${user}/.config/syncthing/key.pem";
 		};
+		tahani-paperless-password = {
+			sopsFile = ../../secrets/tahani-paperless-password;
+			format = "binary";
+		};
 	};
 
 	services.syncthing.settings.folders = {
@@ -139,6 +143,31 @@
 		"Projects/Work" = {
 			path = "/home/${user}/Projects/Work";
 			devices = ["tahani" "chidi"];
+		};
+	};
+
+	services.redis.servers.paperless = {
+		enable = true;
+		port = 6379;
+		bind = "127.0.0.1";
+		settings = {
+			maxmemory = "256mb";
+			maxmemory-policy = "allkeys-lru";
+		};
+	};
+
+	services.paperless = {
+		enable = true;
+		address = "0.0.0.0";
+		passwordFile = config.sops.secrets.tahani-paperless-password.path;
+		settings = {
+			PAPERLESS_DBENGINE = "sqlite";
+			PAPERLESS_REDIS = "redis://127.0.0.1:6379";
+			PAPERLESS_CONSUMER_IGNORE_PATTERN = [
+				".DS_STORE/*"
+				"desktop.ini"
+			];
+			PAPERLESS_OCR_LANGUAGE = "deu+eng";
 		};
 	};
 }
