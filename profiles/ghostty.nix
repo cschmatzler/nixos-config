8ca917d6c2d3cbae416b8b89f91b1a57a5501ca7
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-26 15:18:13 +0100

    ghostty config

--- profiles/ghostty.nix
+++ profiles/ghostty.nix
@@ -1,22 +1,18 @@
 {pkgs, ...}: {
-	programs.ghostty = {
-		enable = true;
-		package = pkgs.emptyDirectory;
-		settings = {
-			command = "${pkgs.fish}/bin/fish";
-			theme = "Catppuccin Latte";
-			window-padding-x = 12;
-			window-padding-y = 3;
-			window-padding-balance = true;
-			font-family = "TX-02 SemiCondensed";
-			font-size = 16.5;
-			cursor-style = "block";
-			mouse-hide-while-typing = true;
-			mouse-scroll-multiplier = 1.25;
-			shell-integration = "detect";
-			shell-integration-features = "no-cursor";
-			clipboard-read = "allow";
-			clipboard-write = "allow";
-		};
-	};
+	xdg.configFile."ghostty/config".text = ''
+		command = ${pkgs.fish}/bin/fish
+		theme = Catppuccin Latte
+		window-padding-x = 12
+		window-padding-y = 3
+		window-padding-balance = true
+		font-family = TX-02
+		font-size = 16.5
+		cursor-style = block
+		mouse-hide-while-typing = true
+		mouse-scroll-multiplier = 1.25
+		shell-integration = detect
+		shell-integration-features = no-cursor
+		clipboard-read = allow
+		clipboard-write = allow
+	'';
 }
