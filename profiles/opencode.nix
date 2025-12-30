024c0b311587bd2fa39df6e6387c58856225cbb2
Christoph Schmatzler <christoph@schmatzler.com>
2025-12-30 10:21:28 +0100

    opencode

--- profiles/opencode.nix
+++ profiles/opencode.nix
@@ -9,8 +9,8 @@
 		settings = {
 			theme = "catppuccin";
 			plugin = [
-				"oh-my-opencode@2.7.2"
-				"opencode-antigravity-auth@1.2.2"
+				"oh-my-opencode"
+				"opencode-antigravity-auth"
 			];
 			instructions = [
 				"CLAUDE.md"
@@ -20,25 +20,6 @@
 			formatter = {
 				mix = {
 					disabled = true;
-				};
-			};
-			command = {
-				deslop = {
-					description = "Remove AI code slop";
-					template = ''
-						Check the diff against main/master, and remove all AI generated slop introduced in this branch.
-						Use jj if available, otherwise git.
-
-						This includes:
-
-						- Extra comments that a human wouldn't add or is inconsistent with the rest of the file
-						- Extra defensive checks or try/catch blocks that are abnormal for that area of the codebase (especially if called by trusted / validated codepaths)
-						- Casts to any to get around type issues
-						- Any other style that is inconsistent with the file
-						- Unnecessary emoji usage
-
-						Report at the end with only a 1-3 sentence summary of what you changed
-					'';
 				};
 			};
 			provider = {
