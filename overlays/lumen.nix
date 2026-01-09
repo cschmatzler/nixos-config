654106232e09899f52acbabb93331ffa02b64bd8
Christoph Schmatzler <christoph@schmatzler.com>
2026-01-09 17:26:23 +0000

    lumen

--- overlays/lumen.nix
+++ overlays/lumen.nix
@@ -1,3 +1,18 @@
-{inputs}: final: prev: {
-	lumen = inputs.lumen.packages.${prev.stdenv.hostPlatform.system}.default;
+{inputs}: final: prev: let
+	manifest = (prev.lib.importTOML "${inputs.lumen}/Cargo.toml").package;
+in {
+	lumen = prev.rustPlatform.buildRustPackage {
+		pname = manifest.name;
+		version = manifest.version;
+
+		cargoLock.lockFile = "${inputs.lumen}/Cargo.lock";
+
+		src = inputs.lumen;
+
+		nativeBuildInputs = [prev.pkg-config];
+		buildInputs = [prev.openssl];
+		OPENSSL_NO_VENDOR = 1;
+
+		doCheck = false;
+	};
 }
