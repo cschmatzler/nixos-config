068b714c75c8eed4edd9ba974db41488243a0d07
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-01 15:36:25 +0000

    use square separators for yazi theme

--- profiles/yazi.nix
+++ profiles/yazi.nix
@@ -10,5 +10,33 @@
 				sort_dir_first = true;
 			};
 		};
+		theme = {
+			tabs = {
+				sep_inner = {
+					open = "";
+					close = "";
+				};
+				sep_outer = {
+					open = "";
+					close = "";
+				};
+			};
+			indicator = {
+				padding = {
+					open = "";
+					close = "";
+				};
+			};
+			status = {
+				sep_left = {
+					open = "";
+					close = "";
+				};
+				sep_right = {
+					open = "";
+					close = "";
+				};
+			};
+		};
 	};
 }
