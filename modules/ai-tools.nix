e58796f771b9235a38370b0b694896e6e3165bf3
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-05 16:49:30 +0000

    disable git-master skill

--- modules/ai-tools.nix
+++ modules/ai-tools.nix
@@ -79,7 +79,7 @@
 			"opencode/oh-my-opencode.json".text =
 				builtins.toJSON {
 					"$schema" = "https://raw.githubusercontent.com/code-yeongyu/oh-my-opencode/master/assets/oh-my-opencode.schema.json";
-					disabled_skills = ["playwright" "dev-browser"];
+					disabled_skills = ["playwright" "dev-browser" "git-master"];
 					git_master = {
 						commit_footer = false;
 						include_co_authored_by = false;
