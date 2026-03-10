71b4e66726098268f630c8848be426374cfa8b98
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-10 10:51:01 +0000

    fmt

--- modules/_neovim/plugins/opencode.nix
+++ modules/_neovim/plugins/opencode.nix
@@ -1,15 +1,17 @@
 {pkgs, ...}: let
-	opencode-nvim = pkgs.vimUtils.buildVimPlugin {
-		pname = "opencode-nvim";
-		version = "unstable-2026-03-07";
-		src = pkgs.fetchFromGitHub {
-			owner = "sudo-tee";
-			repo = "opencode.nvim";
-			rev = "dffa3f39a8251c7ba53b1544d8536b5f51b4e90d";
-			hash = "sha256-KxIuToMpyo/Yi4xKirMV8Fznlma6EL1k4YQm5MQdGw4=";
+	opencode-nvim =
+		pkgs.vimUtils.buildVimPlugin {
+			pname = "opencode-nvim";
+			version = "unstable-2026-03-07";
+			src =
+				pkgs.fetchFromGitHub {
+					owner = "sudo-tee";
+					repo = "opencode.nvim";
+					rev = "dffa3f39a8251c7ba53b1544d8536b5f51b4e90d";
+					hash = "sha256-KxIuToMpyo/Yi4xKirMV8Fznlma6EL1k4YQm5MQdGw4=";
+				};
+			doCheck = false;
 		};
-		doCheck = false;
-	};
 in {
 	programs.nixvim = {
 		extraPlugins = with pkgs.vimPlugins; [
