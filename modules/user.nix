b0e09848a247bf8d0d99cf36e7be38a58248ef19
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-18 10:25:19 +0000

    fix home

--- modules/user.nix
+++ modules/user.nix
@@ -1,8 +1,20 @@
-{den, ...}: {
+{den, lib, ...}: {
 	den.aspects.cschmatzler.includes = [
 		den.provides.primary-user
 		den.aspects.shell
 		den.aspects.ssh-client
+		den.aspects.terminal
+		den.aspects.email
+		den.aspects.atuin
+		den.aspects.dev-tools
+		den.aspects.neovim
+		den.aspects.ai-tools
+		den.aspects.secrets
+		den.aspects.zellij
+		den.aspects.zk
+		({host, ...}: lib.optionalAttrs (host.class == "darwin") {
+			includes = [den.aspects.desktop];
+		})
 	];
 
 	den.aspects.cschmatzler.homeManager = {
