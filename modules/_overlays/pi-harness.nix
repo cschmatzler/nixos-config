aa83a961bde8e26ea87778eebe91221e0f19facc
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-26 19:57:44 +0100

    fix

--- modules/_overlays/pi-harness.nix
+++ modules/_overlays/pi-harness.nix
@@ -12,7 +12,7 @@
 					src = inputs.pi-harness;
 					pnpm = prev.pnpm_10;
 					fetcherVersion = 1;
-					hash = "sha256-dXctaspB+efF0o88AB+1JPyWg36ZT90VeagPEBHlF20=";
+					hash = "sha256-WLbMkXERpInK/cUoRqUewwd1R6uXC/ZFDtg5wPSQJd0=";
 				};
 
 			nativeBuildInputs = [
