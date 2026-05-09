b12a2599cd77ef9123d49b7e3ffaeb2dfabcd1fa
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-09 13:58:28 +0000

    Fix Diffview changed-line colors

--- modules/_neovim/autocmd.nix
+++ modules/_neovim/autocmd.nix
@@ -30,6 +30,8 @@
 						vim.api.nvim_set_hl(0, "WhichKeyTitle", { fg = p.foam, bg = p.base, bold = true })
 						vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = p.iris, bg = p.base })
 						vim.api.nvim_set_hl(0, "NoiceCmdlineIcon", { fg = p.foam, bg = p.base })
+						vim.api.nvim_set_hl(0, "DiffviewDiffChange", { fg = p.text, bg = p.highlight_low })
+						vim.api.nvim_set_hl(0, "DiffviewDiffText", { fg = p.text, bg = p.highlight_med, bold = true })
 					end
 				'';
 			}
