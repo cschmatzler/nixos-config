5aef61458087c02d9de00a163a7d69db8d8808a0
Christoph Schmatzler <christoph@schmatzler.com>
2026-05-12 22:07:51 +0200

    Fix vite-plus pnpm deps hash on Darwin

--- modules/_overlays/vite-plus.nix
+++ modules/_overlays/vite-plus.nix
@@ -4,7 +4,15 @@ in
 	vitePlusOverlay
 	// {
 		vite-plus =
-			vitePlusOverlay.vite-plus.overrideAttrs (_: {
+			vitePlusOverlay.vite-plus.overrideAttrs (old: {
 					doInstallCheck = false;
+					pnpmDeps =
+						if final.stdenv.hostPlatform.isDarwin
+						then
+							old.pnpmDeps.override {
+								pnpm = final.pnpm_10;
+								hash = old.pnpmDeps.outputHash;
+							}
+						else old.pnpmDeps;
 				});
 	}
