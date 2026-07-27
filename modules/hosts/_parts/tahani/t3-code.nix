b80856ec56d004aa3a00250fd1f715cbed5de540
Christoph Schmatzler <christoph@schmatzler.com>
2026-07-27 07:42:50 +0000

    t3

--- modules/hosts/_parts/tahani/t3-code.nix
+++ modules/hosts/_parts/tahani/t3-code.nix
@@ -7,7 +7,7 @@
   home = local.mkHome pkgs.stdenv.hostPlatform.system;
   apiKeyPath = local.secretPath "opencode-api-key";
   homeAssistantTokenPath = local.secretPath "home-assistant-token";
-  version = "0.0.29-nightly.20260724.893";
+  version = "0.0.29-nightly.20260727.915";
 in {
   sops.secrets = {
     home-assistant-token.restartUnits = ["t3-code.service"];
