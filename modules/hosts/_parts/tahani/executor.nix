b3f04fe4fa062839c4a474fc848cfb70c6ad5160
Christoph Schmatzler <christoph@schmatzler.com>
2026-08-26 14:41:39 +0000

    Upgrade executor to 1.6.0

--- modules/hosts/_parts/tahani/executor.nix
+++ modules/hosts/_parts/tahani/executor.nix
@@ -23,7 +23,7 @@
   virtualisation.oci-containers = {
     backend = "docker";
     containers.executor = {
-      image = "ghcr.io/usefulsoftwareco/executor-selfhost:1.5.42";
+      image = "ghcr.io/usefulsoftwareco/executor-selfhost:1.6.0";
       pull = "always";
       ports = ["127.0.0.1:4788:4788"];
       volumes = ["/var/lib/executor:/data"];
