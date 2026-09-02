ee8212044a2553efad7484989bbbc1d48465aeff
Christoph Schmatzler <christoph@schmatzler.com>
2026-09-02 19:18:52 +0200

    Add Paper MCP service for executor (#14)
    * Add Paper MCP service for executor
    
    * Add Taste design profile app to Janet

--- modules/hosts/janet.nix
+++ modules/hosts/janet.nix
@@ -2,6 +2,8 @@
   den.aspects.janet = {
     includes = [
       den.aspects.host-darwin-base
+      den.aspects.paper
+      den.aspects.taste
       den.aspects.t3code
       den.aspects.syncthing
     ];
